25b2a25f553f8bebbf1102177950857094427725
Liheng.Gong <liheng.gong@hotmail.com>
2018-07-16 16:18:42 -0400

    delete redundant files

--- Demo1/django_liveresource/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from django_liveresource import views
-
-urlpatterns = patterns('',
-	url(r'^multi/$', views.multi, name='multi'),
-	url(r'^updates/$', views.updates, name='updates'),
-)
