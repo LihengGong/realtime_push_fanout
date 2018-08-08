b03d7a30a4a939d5897060fc8aa3281a9e3517fb
Liheng.Gong <liheng.gong@hotmail.com>
2018-08-08 17:12:17 -0400

    adding websocket send and receive code

--- QueryPushpinStat/server/testWhile/consumers.py
+++ QueryPushpinStat/server/testWhile/consumers.py
@@ -45,15 +45,16 @@ class StatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         # TODO: add real receive code here
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print('in StatConsumer.receive')
+        # text_data_json = json.loads(text_data)
+        # message = text_data_json['message']
+        print('in StatConsumer.receive, text_data: ', text_data)
+        # self.send('from server')
 
         self.send(text_data=json.dumps({
-            'message': message
+            'message': 'from server'
         }))
 
     # "def send(self, text_data=None, bytes_data=None, close=False):"
-    def send(self, bytes_data):
+    def send1(self, bytes_data):
         # TODO: add real send code here
         print('in StatConsumer.send')
